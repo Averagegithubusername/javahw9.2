83b55a8daf0047daf6f1046680aa7b8ba88c6f03
Averagegithubusername <s0mething_different@mail.ru>
2022-01-03 18:26:44 +0300

    rework

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,19 +5,10 @@ import ru.netology.domain.Post;
 public class PostManager {
     private Post[] posts;
 
-    public Post[] wallSearch() {
-        int getOwnerId;
-        int getDomain;
-        int getOwnersOnly;
-        int getOffset;
-        int count;
-        long[] items;
-
+    public Post[] wallSearch(int getOwnerId, int getDomain, int getOwnersOnly, int getOffset) {
         return null;
     }
 
-    public void wallDelete() {
-        int getOwnerId;
-        int getPostId;
+    public void wallDelete(int getOwnerId, int getPostId) {
     }
 }
